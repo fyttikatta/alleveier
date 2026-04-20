a7a2de3dcd50d09d75ac118563a4c8e2969b5cb6
fyttikatta <thomassen92@hotmail.com>
2026-04-20 18:57:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'alleveier-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  // Network-first for API calls
+  if (url.hostname !== location.hostname) {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  // Cache-first for app shell
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
